17f65b2dffd94afd96426ff4ec21ee7606e49dd6
Scott <scotthenry1@gmail.com>
2023-04-17 14:49:55 -0400

    Created initial models and db

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-from config import *
+from config import app, db, api
 from models import Customer, Movie, Rental
 
 
--- server/models.py
+++ server/models.py
@@ -5,15 +5,6 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 from config import db
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(constraint_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
-
-db = SQLAlchemy(metadata=metadata)
-
 class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
     
