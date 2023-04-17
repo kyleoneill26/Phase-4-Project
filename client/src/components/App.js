8c070d3470b771f84a9ed30bebaeb407ec242307
Scott <scotthenry1@gmail.com>
2023-04-17 16:25:52 -0400

    Adding React files

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  // Code goes here!
+  <Route exact path="/">
+    <Home users={users} setUsers={setUsers} friendships={friendships} setFriendships={setFriendships} posts={posts} setPosts={setPosts} selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+  </Route>
 }
 
 export default App;
