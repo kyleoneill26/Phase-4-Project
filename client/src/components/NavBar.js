9c23f0bb86306c356764a76954dd254600ece92d
Jesse Hunter <jjsbizarreadventure@yahoo.com>
2023-04-18 13:22:25 -0500

    small no branch oopsies

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
-import '..index.css'
+
 
 
 export default function NavBar() {
     return(
         <nav classname=''>
-            <a href= "">we can put a log of some sort here</>
+            <a href= "">we can put a log of some sort here</a>
             <NavLink classname='NavLink' exact to = './'>Home</NavLink>
             <NavLink className="NavLink" to = "/movies">Movies</NavLink>
             <NavLink classname='NavLink' to = '/account'>Account</NavLink>
