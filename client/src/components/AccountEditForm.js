b15a67ef6cec7efaa460a9bafcc9aeb25cce173a
Jesse Hunter <jjsbizarreadventure@yahoo.com>
2023-04-19 13:03:30 -0500

    adding more functions but forgot my branch again

--- client/src/components/AccountEditForm.js
+++ client/src/components/AccountEditForm.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
 
-function AccountEditForm() {
+
+
+function AccountEditForm({ handleHideCustomerEditForm,  }) {
+
+
+    ////// WE NEED TO PASS THE FILTERED USER DATA TO THIS FUNCTION ///// TODO
+    const [ fname, setFName ] = useState()
+    const [ lname, setLName ] = useState()
+    // const [ password, setPassword ] = useState() should we make a password change form?
+    const [ age, setAge ] = useState()
+    const [ phone, setPhone ] = useState()
+    const [ address, setAddress ] = useState()
+
+    const handleFName = (e) => {setFName(e.target.value)}
+    const handleLName = (e) => {setLName(e.target.value)}
+    // const handlePassword = (e) =>{setPassword(e.target.value)}
+    const handleAge = (e) => {setAge(e.target.value)}
+    const handlePhone = (e) => {setPhone(e.target.value)}
+    const handleAddress = (e) => {setAddress(e.target.value)}
+
+
+
+
+
     return (
-        <div>AccountEditForm</div>
+        <Form className='editForm' onSubmit={handlePatchSubmit}>
+            <div>
+                <Form.Control onChange={handleTitle} type='text' name='title' placeholder='Title'/>
+                <Form.Control onChange={handleImage} type='text' name='image' placeholder='Image'/>
+                <Form.Control onChange={handleGenre} type='text' name='genre' placeholder='Genre'/>
+                <Form.Control onChange={handleYear} type='text' name='year' placeholder='Year'/>
+                <Form.Control onChange={handleRating} type='text' name='rating' placeholder='Rating'/>
+            </div>
+            <Button className='add-button' type='submit'>Add A Movie</Button>
+            <Button className='add-button' onClick={handleHideMovieForm}>Close Form</Button>
+        </Form>
     )
 }
 
--- client/src/components/AccountPage.js
+++ client/src/components/AccountPage.js
@@ -1,6 +1,28 @@
 import React from 'react'
+import AccountEditForm from './AccountEditForm'
 
 function AccountPage() {
+
+
+
+    const customerComponents = customer.map() // TODO
+
+    const [hideCustomerEditForm, setHideCustomerEditForm] = useState(true)
+    const handleHideCustomerEditForm = () => {
+        setHideCustomerEditForm(hideCustomerEditForm => !hideCustomerEditForm)
+    } // FIXME 
+
+    function FormButton({handleHideCustomerEditForm}) {
+        return(
+            <button onClick={handleHideCustomerEditForm} className="hideFormButton">Edit Account Info</button>
+        )
+    }
+
+
+
+
+
+
     return (
         <div>AccountPage</div>
     )
